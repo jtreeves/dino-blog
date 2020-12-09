f384434060bd34e7eee8f716a3bd691e30ad4046
jtreeves <jackson.t.reeves@gmail.com>
2020-12-09 12:24:47 -0500

    [App] Move Dino info into main file, and add comments

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import './App.css'
-import Dino from './Dino'
+// import Dino from './Dino'
+import React, { Component } from 'react'
 
-function App(props) {
-    console.log(props.post)
-    return (
-        <div className="App">
-            <Dino
-                title={props.post.title}
-                author={props.post.author}
-                body={props.post.body}
-                comments={props.post.comments}
-            />
-        </div>
-    )
+class App extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            title: props.post.title,
+            author: props.post.author,
+            body: props.post.body,
+            comments: props.post.comments,
+            // inputText: ''
+        }
+    }
+
+    newComment = () => {
+        let newComments = [...this.state.comments, this.state.inputText]
+        this.setState({ comments: newComments })
+    }
+
+    changeInput = (event) => {
+        this.setState({ inputText: event.target.value })
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>{this.state.title}</h1>
+                <p>by {this.state.author}</p>
+                <p>{this.state.body}</p>
+                <h2>Comments:</h2>
+                <p>{this.state.comments}</p>
+                <input type='text' value={this.state.inputText} onChange={this.changeInput} />
+                <button onClick={this.newComment}>Add Comment</button>
+            </div>
+        )
+    }
 }
 
 export default App
--- src/Dino.js
+++ src/Dino.js
@@ -1,14 +1,28 @@
-function Dino(props) {
-    console.log(props)
-    return (
-        <div>
-            <h1>{props.title}</h1>
-            <p>by {props.author}</p>
-            <p>{props.body}</p>
-            <h2>Comments:</h2>
-            <p>{props.comments[0]}</p>
-        </div>
-    )
+import React, { Component } from 'react'
+
+class Dino extends Component {
+    constructor() {
+        super()
+        this.state = {
+            title: this.props.title,
+            author: this.props.author,
+            body: this.props.body,
+            comments: this.props.comments,
+            inputtedText: ''
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>{this.title}</h1>
+                <p>by {this.author}</p>
+                <p>{this.body}</p>
+                <h2>Comments:</h2>
+                <p>{this.comments[0]}</p>
+            </div>
+        )
+    }
 }
 
 export default Dino
